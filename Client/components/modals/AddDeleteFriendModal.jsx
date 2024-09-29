97170aed73e6908e0ddf3e0146e185c529ed1f6c
migue <114624729+Miguecaritatriste@users.noreply.github.com>
2024-09-29 15:17:34 +0200

    [FIX] AddDeleteFriendModal closing when tapping outside, but not when tapping inside

--- Client/components/modals/AddDeleteFriendModal.jsx
+++ Client/components/modals/AddDeleteFriendModal.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Modal, View, Text, TextInput, TouchableOpacity, Animated, Pressable, Alert } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
 import tw from 'twrnc';
 import { useThemeColor } from '../../hooks/useThemeColor';
 
@@ -77,18 +76,26 @@ const AddDeleteFriendModal = ({ setModalVisible, modalVisible, OnAccept, selecte
     >
       <Pressable
         style={tw`flex-1 justify-center items-center bg-black bg-opacity-50`}
+        onPress={() => setModalVisible(false)}
       >
-        <Animated.View style={[tw`w-11/12 max-w-md bg-${modal_bg_color} rounded-lg shadow-lg p-6`, slideIn]}>
+        <Animated.View
+          style={[tw`w-11/12 max-w-md bg-${modal_bg_color} rounded-lg shadow-lg p-6`, slideIn]}
+          onStartShouldSetResponder={() => true}
+        >
           <Text style={tw`text-2xl font-semibold mb-4 text-${modal_title_color} text-center`}>{getTitle()}</Text>
           <Text style={tw`text-lg font-semibold mb-4 text-${modal_text_color} text-center`}>@{selectedUser?.name}?</Text>
-          <TextInput
-            style={tw`w-full bg-gray-200 p-2 rounded-lg mb-2`}
-            placeholder="Escribe tu mensaje..."
-            value={message}
-            maxLength={maxLength}
-            onChangeText={handleChangeText}
-          />
-          <Text style={tw`text-right text-${modal_text_color} mb-4`}>{charCount}/{maxLength}</Text>
+          {action == 'add' && (
+            <>
+              <TextInput
+                style={tw`w-full bg-gray-200 p-2 rounded-lg mb-2`}
+                placeholder="Escribe tu mensaje..."
+                value={message}
+                maxLength={maxLength}
+                onChangeText={handleChangeText}
+              />
+              <Text style={tw`text-right text-${modal_text_color} mb-4`}>{charCount}/{maxLength}</Text>
+            </>
+          )}
           <View style={tw`flex-row justify-between w-full mt-auto`}>
             <TouchableOpacity
               style={tw`flex-1 bg-${Modal_cancel_button} p-2 rounded-full mx-1`}
