36c0e794f7ae5630eb7f3c141751a3c114167fc7
ChanguaZzz <ggualdronarjona@gmail.com>
2024-10-14 20:13:34 +0200

    send audiodata

--- Server/PushNotifications/AudioNotification.ts
+++ Server/PushNotifications/AudioNotification.ts
@@ -21,7 +21,7 @@ export async function AudioNotification(senderUsername: string , token: string, 
     sound: 'default',
     title: `@${senderUsername} is currently speaking.`,
     body: "Walkie Talkie",
-    data: {data : `goes herefsfsef`}
+    data: {data : `${audioData}`}
     // Asegúrate de que la notificación se muestre en primer plano
   });
 
