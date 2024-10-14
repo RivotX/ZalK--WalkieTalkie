f94329ab2fc54c1496fc3fe6ce503ac008ff2025
RivotX <miguelrosso60@gmail.com>
2024-10-14 14:15:12 +0200

    force reverted

--- Server/App.ts
+++ Server/App.ts
@@ -1507,7 +1507,7 @@ const initialRooms = [
   { name: "Cocina Vegana", info: "Comparte recetas veganas." },
 ];
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ alter: true }).then(() => {
   server.listen(3000, async () => {
     console.log("Server running...");
 
