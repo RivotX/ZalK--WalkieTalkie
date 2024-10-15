c4ce23510c166cacadc07f8d99f7a9fdadeebceb
RivotX <miguelrosso60@gmail.com>
2024-10-15 14:10:00 +0200

    Centered MiniModal animation

--- Client/components/modals/UserProfileMiniModal.jsx
+++ Client/components/modals/UserProfileMiniModal.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState, useRef } from "react";
-import { TouchableOpacity, Modal, Image, Text, View, Animated, Easing, Dimensions } from "react-native";
-import tw from "twrnc";
-import ProfileIcon from "../../assets/images/images.png";
-import groupicon from "../../assets/images/groupicon.png";
-import { useThemeColor } from "../../hooks/useThemeColor";
-import { useLanguage } from "../../context/LanguageContext";
-import axios from "axios";
-import getEnvVars from "../../config";
+import React, { useEffect, useState, useRef } from 'react';
+import { TouchableOpacity, Modal, Image, Text, View, Animated, Easing, Dimensions } from 'react-native';
+import tw from 'twrnc';
+import ProfileIcon from '../../assets/images/images.png';
+import groupicon from '../../assets/images/groupicon.png';
+import { useThemeColor } from '../../hooks/useThemeColor';
+import { useLanguage } from '../../context/LanguageContext';
+import axios from 'axios';
+import getEnvVars from '../../config';
 const { SERVER_URL } = getEnvVars();
 import { useNavigation } from '@react-navigation/native';
 
 const UserProfileMiniModal = ({ user, modalIconVisible, setModalIconVisible, iconSize, isContact, initialPosition }) => {
-  const textColor = useThemeColor({}, "text");
+  const textColor = useThemeColor({}, 'text');
   const [qty, setQty] = useState(null);
   const [members, setMembers] = useState([]);
   const { Texts } = useLanguage();
@@ -19,49 +19,55 @@ const UserProfileMiniModal = ({ user, modalIconVisible, setModalIconVisible, ico
   const [hideInfo, setHideInfo] = useState(true);
   const [modalOpacity, setModalOpacity] = useState(0);
   const [isClosing, setIsClosing] = useState(false);
+  const borderRadius = useRef(new Animated.Value(0)).current;
+  console.log('UserProfileMiniModal: ', user);
+  const navigation = useNavigation();
+  const UserProfileMiniModal_BG = useThemeColor({}, 'UserProfileMiniModal_BG');
+  // Animation values
   const position = useRef(new Animated.ValueXY({ x: initialPosition?.x || 0, y: (initialPosition?.y || 0) - 28 })).current;
   const size = useRef(new Animated.Value(56)).current;
-  const borderRadius = useRef(new Animated.Value(0)).current;
-  console.log("UserProfileMiniModal: ", user);
-  const navigation = useNavigation();
-  const UserProfileMiniModal_BG = useThemeColor({}, "UserProfileMiniModal_BG");
+  const screenWidth = Dimensions.get('window').width;
+  const screenHeight = Dimensions.get('window').height;
+  const pictureWidth = 250;
+  const centerX = screenWidth / 2 - pictureWidth/2;
+  const centerY = (screenHeight - 56) / 2 / 4;
+  console.log('Screen width: ', screenWidth);
+  console.log('Center X: ', centerX);
 
   // Fetch group members
   useEffect(() => {
     if (!isContact) {
-      axios.post(`${SERVER_URL}/getGroupMembers`, { groupId: user.id }).then((res) => {
-        console.log("Members: ", res.data);
-        setQty(res.data.length);
-        setMembers(res.data);
-      }).catch((error) => {
-        console.log(error);
-      });
+      axios
+        .post(`${SERVER_URL}/getGroupMembers`, { groupId: user.id })
+        .then((res) => {
+          console.log('Members: ', res.data);
+          setQty(res.data.length);
+          setMembers(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
   }, [user]);
 
   // ==== Open Modal animation ====
   useEffect(() => {
     if (modalIconVisible && initialPosition) {
-
       setModalOpacity(50);
-      const screenWidth = Dimensions.get('window').width;
-      const screenHeight = Dimensions.get('window').height;
 
-      const centerX = ((screenWidth - 56) / 2) / 2;
-      const centerY = ((screenHeight - 56) / 2) / 4;
 
       Animated.parallel([
         Animated.timing(position, {
           toValue: {
             x: centerX,
-            y: centerY
+            y: centerY,
           },
           duration: 300,
           easing: Easing.out(Easing.ease),
           useNativeDriver: false,
         }),
         Animated.timing(size, {
-          toValue: 268,
+          toValue: pictureWidth,
           duration: 300,
           easing: Easing.out(Easing.ease),
           useNativeDriver: false,
@@ -82,7 +88,7 @@ const UserProfileMiniModal = ({ user, modalIconVisible, setModalIconVisible, ico
 
     setIsClosing(true);
     setModalOpacity(0);
-    console.log("Closing modal");
+    console.log('Closing modal');
     setHideText(true);
     setHideInfo(true);
 
@@ -90,7 +96,7 @@ const UserProfileMiniModal = ({ user, modalIconVisible, setModalIconVisible, ico
       Animated.timing(position, {
         toValue: {
           x: initialPosition.x,
-          y: initialPosition.y - 29
+          y: initialPosition.y - 29,
         },
         duration: 200,
         easing: Easing.out(Easing.ease),
@@ -109,7 +115,7 @@ const UserProfileMiniModal = ({ user, modalIconVisible, setModalIconVisible, ico
         useNativeDriver: false,
       }),
     ]).start(() => {
-      console.log("Reverse animation completed");
+      console.log('Reverse animation completed');
       setModalIconVisible(false);
       setHideText(false);
       setHideInfo(false);
@@ -118,7 +124,7 @@ const UserProfileMiniModal = ({ user, modalIconVisible, setModalIconVisible, ico
   };
   // ==== Handle profile image press ====
   const handleProfileImagePress = () => {
-    navigation.navigate("ProfilePictureScreen", { user, isContact });
+    navigation.navigate('ProfilePictureScreen', { user, isContact });
     setModalIconVisible(false);
     setHideText(false);
     setHideInfo(false);
@@ -126,16 +132,12 @@ const UserProfileMiniModal = ({ user, modalIconVisible, setModalIconVisible, ico
 
   return (
     <Modal animationType="fade" transparent={true} visible={modalIconVisible} onRequestClose={handleCloseModal}>
-      <TouchableOpacity
-        style={tw`flex-1 justify-start items-center bg-black bg-opacity-${modalOpacity}`}
-        activeOpacity={1}
-        onPress={handleCloseModal}
-      >
+      <TouchableOpacity style={tw`flex-1 justify-start items-center bg-black bg-opacity-${modalOpacity}`} activeOpacity={1} onPress={handleCloseModal}>
         <Animated.View style={[tw`absolute`, { top: position.y, left: position.x, width: size, height: size }]}>
           {/* Profile image */}
           <TouchableOpacity onPress={handleProfileImagePress}>
             <Animated.Image
-              style={[{ width: size, height: size, resizeMode: "cover", borderRadius }]}
+              style={[{ width: size, height: size, resizeMode: 'cover', borderRadius }]}
               source={user.profile ? { uri: user.profile } : isContact ? ProfileIcon : groupicon}
             />
           </TouchableOpacity>
@@ -155,9 +157,7 @@ const UserProfileMiniModal = ({ user, modalIconVisible, setModalIconVisible, ico
           {!hideText && members.length != 0 && (
             <Animated.View style={[tw`bg-black bg-opacity-10 p-3 mt-2`, { width: size }]}>
               <Text style={tw`text-[${textColor}] text-center mt-4`}>
-                <Text style={tw`text-[${textColor}] text-center font-bold`}>
-                  {qty != null && `${Texts.Members} : ${qty}`}
-                </Text>
+                <Text style={tw`text-[${textColor}] text-center font-bold`}>{qty != null && `${Texts.Members} : ${qty}`}</Text>
               </Text>
               {members.map((member, index) => (
                 <Text key={index} style={tw`text-[${textColor}] text-center`}>
