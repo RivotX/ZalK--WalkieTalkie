b044ae2ac448e0b16e6dbe117751928cb74eb7b7
ChanguaZzz <ggualdronarjona@gmail.com>
2024-10-22 19:45:17 +0200

    force bbd

--- Server/App.ts
+++ Server/App.ts
@@ -1660,14 +1660,14 @@ const initialRooms = [
   { name: 'Cocina Vegana', info: 'Comparte recetas veganas.' },
 ];
 
-sequelize.sync({ alter: true }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   server.listen(3000, async () => {
     console.log('Server running...');
 
-    // // create groups
-    // for (const room of initialRooms) {
-    //   await Rooms.upsert(room);
-    //   console.log("Room created:", room);
-    // }
+    // create groups
+    for (const room of initialRooms) {
+      await Rooms.upsert(room);
+      console.log("Room created:", room);
+    }
   });
 });
