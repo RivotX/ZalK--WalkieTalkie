8430ad093de45ff83b95d8e0c76eecf5d20e93e6
RivotX <miguelrosso60@gmail.com>
2024-10-24 16:37:01 +0200

    El Idioma seleccionado se guarda en la memoria del dispositivo

--- Client/context/LanguageContext.js
+++ Client/context/LanguageContext.js
@@ -1,5 +1,6 @@
 // Client/context/LanguageContext.js
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Texts } from '../constants/Texts';
 
 const LanguageContext = createContext();
@@ -7,8 +8,29 @@ const LanguageContext = createContext();
 export const LanguageProvider = ({ children }) => {
   const [language, setLanguage] = useState('en');
 
-  const toggleLanguage = () => {
-    setLanguage((prevLanguage) => (prevLanguage === 'en' ? 'es' : 'en'));
+  useEffect(() => {
+    const loadLanguage = async () => {
+      try {
+        const storedLanguage = await AsyncStorage.getItem('language');
+        if (storedLanguage) {
+          setLanguage(storedLanguage);
+        }
+      } catch (error) {
+        console.error('Failed to load language from storage', error);
+      }
+    };
+
+    loadLanguage();
+  }, []);
+
+  const toggleLanguage = async () => {
+    const newLanguage = language === 'en' ? 'es' : 'en';
+    setLanguage(newLanguage);
+    try {
+      await AsyncStorage.setItem('language', newLanguage);
+    } catch (error) {
+      console.error('Failed to save language to storage', error);
+    }
   };
 
   return (
